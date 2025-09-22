05150d8c0e392715e573b3e00dbaeef162df3514
vip-only <lysaraz227@gmail.com>
2025-09-22 16:34:47 +0300

    correction html contrat pdf, supp method contratService

--- src/main/java/com/example/gestion_rh/service/ContratEssaiService.java
+++ src/main/java/com/example/gestion_rh/service/ContratEssaiService.java
@@ -4,12 +4,6 @@ import org.springframework.stereotype.Service;
 
 import com.example.gestion_rh.model.ContratEssai;
 import com.example.gestion_rh.repository.ContratEssaiRepository;
-import com.example.gestion_rh.model.Candidat;
-import com.example.gestion_rh.repository.CandidatRepository;
-import com.example.gestion_rh.model.Poste;
-import com.example.gestion_rh.repository.PosteRepository;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 
 import java.util.List;
 
@@ -17,13 +11,9 @@ import java.util.List;
 public class ContratEssaiService {
 
     private final ContratEssaiRepository repository;
-    private final CandidatRepository candidatRepository;
-    private final PosteRepository posteRepository;
 
-    public ContratEssaiService(ContratEssaiRepository repository, CandidatRepository candidatRepository, PosteRepository posteRepository) {
+    public ContratEssaiService(ContratEssaiRepository repository) {
         this.repository = repository;
-        this.candidatRepository = candidatRepository;
-        this.posteRepository = posteRepository;
     }
 
     public ContratEssai creerContrat(ContratEssai contrat) {
@@ -46,25 +36,4 @@ public class ContratEssaiService {
     public void supprimerContrat(Long id) {
         repository.deleteById(id);
     }
-    // ...existing code...
-        // public void creerContratPourCandidat(int candidatId) {
-        //     Candidat candidat = candidatRepository.findById(candidatId).orElseThrow();
-        //     ContratEssai contrat = new ContratEssai();
-        //     contrat.setCandidat(candidat);
-        //     Poste poste = posteRepository.findAll().stream().findFirst().orElse(null);
-        //     contrat.setPoste(poste);
-        //     int i = 0;
-        //     ContratEssai c = this.listerContrats().get(this.listerContrats().size()-1);
-        //     LocalDateTime d = c.getDateFin();
-        //     contrat.setDateDebut(d.plusHours(2));
-        //     contrat.setDateFin(contrat.getDateDebut().plusHours(1));
-        //     contrat.setDuree(30);
-        //     contrat.setEtat("En attente");
-        //     repository.save(contrat);
-
-        //     // Mettre à jour estPropose à true
-        //     candidat.setEstPropose(true);
-        //     candidatRepository.save(candidat);
-        // }
-    // ...existing code...
 }
